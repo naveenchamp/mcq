f106bd8806d99ee967b0d59b975cac86049d3ca8
Naveen reddy tippasani <162026696+naveenchamp@users.noreply.github.com>
2025-07-01 00:13:51 +0530

    Update mcq.js

--- mcq.js
+++ mcq.js
@@ -1,1 +1,39 @@
+let questionsForm = document.getElementById("questionsForm");
+let cityHyderabad = document.getElementById("cityHyderabad");
+let cityDelhi = document.getElementById("cityDelhi");
+let cityChennai = document.getElementById("cityChennai");
+let cityMumbai = document.getElementById("cityMumbai");
+let submitBtn = document.getElementById("submitBtn");
+let resultMsg = document.getElementById("resultMsg");
+let dange = "Delhi";
+let dange1 = null;
 
+cityHyderabad.addEventListener("change", function(event) {
+    dange1 = event.target.value;
+});
+cityDelhi.addEventListener("change", function(event) {
+    dange1 = event.target.value;
+});
+cityMumbai.addEventListener("change", function(event) {
+    dange1 = event.target.value;
+});
+cityChennai.addEventListener("change", function(event) {
+    dange1 = event.target.value;
+});
+
+function runtheFunction() {
+    if (dange1 === dange) {
+        resultMsg.textContent = "Correct Answer!!";
+        resultMsg.style.color = "#2b9a40";
+    } else if (dange1 === null) {
+        resultMsg.textContent = "please select the Answer";
+        resultMsg.classList.add('returmsg');
+    } else {
+        resultMsg.textContent = "Wrong Answer!";
+        resultMsg.classList.add('returmsg');
+    }
+}
+questionsForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    runtheFunction();
+});
